4e319a5d93b533614d01e4a5fb28faf93d9c265f
Ray C Horn <raychorn@gmail.com>
2020-10-15 14:24:52 -0600

    08-07-2006a

--- gitify.sh
+++ gitify.sh
@@ -5,5 +5,5 @@ find * -size +4M -type f -print >> .gitignore
 git add -A
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://raychorn:2d4c1265065d163f5fb5458eaee1d717866bfc1d@github.com/raychorn/svn_PastaPomodoro.git
+git remote add origin https://raychorn:c26beca76190a7b64b0f6ec36029745f146a1853@github.com/raychorn/svn_PastaPomodoro.git
 git push -u origin main
